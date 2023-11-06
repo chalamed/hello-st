cd4260d754d894c8f24a6fac4212b501ef711812
chalamed <149961946+chalamed@users.noreply.github.com>
2023-11-05 23:44:19 -0500

    ChatPdfRq.py

--- Hello.py
+++ Hello.py
@@ -14,37 +14,69 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import pinecone
+from langchain.vectorstores import Pinecone
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.callbacks import get_openai_callback
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from streamlit_extras.add_vertical_space import add_vertical_space
+from dotenv import load_dotenv
 
+
+import os
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+
+load_dotenv()
 LOGGER = get_logger(__name__)
 
+with st.sidebar:
+    st.title("👀 L'art de rendre les documents vivants 👀")
+    st.markdown('''
+    ## About APP:
 
+    Changeons nos abitudes
+
+    ## About me:
+
+    - [Linkedin](Chala Mohamed)
+
+    ''')
+
+    add_vertical_space(4)
+    st.write("💬 Hier n'est plus, demain n'est pas encore. Nous n'avons qu'aujourd'hui. Commençons. 🤗")
+    
 def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    st.header("🧐 Faire parler les documents c'est possible 🧐")
 
-    st.write("# Welcome to Streamlit! 👋")
 
-    st.sidebar.success("Select a demo above.")
+    PINECONE_API_KEY = '580508ed-95a6-4500-844a-e07f37ca7657'
+    PINECONE_ENV = 'gcp-starter'  # os.getenv('PINECONE_ENV')
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    pinecone.init(api_key=PINECONE_API_KEY, environment='gcp-starter')
+    index = pinecone.Index('indexrq')
+    embeddings = OpenAIEmbeddings()
+    vectordb = Pinecone.from_existing_index(index_name=index, embedding=embeddings, namespace=PINECONE_ENV)
+
+    query = st.text_input("Ask questions about related your upload pdf file")
+    # st.write(query)
+
+    if query:
+        # openai chat process
+        #llm = OpenAI(temperature=0.8)
+        #chain = load_qa_chain(llm=llm, chain_type="stuff")
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        pdf_chat = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.8), vectordb.as_retriever(),
+                                                            memory=memory)
+
+        with get_openai_callback() as cb:
+            #response = chain.run(input_documents=docs, question=query)
+            response = pdf_chat({"question": query})
+            print(cb)
+        st.write(response["answer"])
 
 
 if __name__ == "__main__":
